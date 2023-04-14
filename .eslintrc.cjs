63b2fd4e4ce79a99512a8a76cadcab8be06c1841
J-ln <julianrolon7@gmail.com>
2023-04-13 23:00:30 -0300

    asd

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,6 +30,9 @@ const config = {
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
   },
+  "exclude": [
+    "./tailwind.config.js"
+  ],
 };
 
 module.exports = config;
